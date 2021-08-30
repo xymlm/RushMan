925f0438bf656fd661218e7958be4070bdfa63da
xymlm <2463248361@qq.com>
2021-08-30 18:28:04 +0800

    begin

---
+++ src/main/java/com/xym/rush/RushManApplication.java
@@ -0,0 +1,13 @@
+package com.xym.rush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RushManApplication.class, args);
+    }
+
+}
